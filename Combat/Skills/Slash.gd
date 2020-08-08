cbd7f997fc8586fd7bfc7b1ee1d99bc290b6326f
Eren Oğuz <65904273+Threnkost@users.noreply.github.com>
2020-08-08 23:45:33 +0300

    Add files via upload

---
+++ Combat/Skills/Slash.gd
@@ -0,0 +1,27 @@
+extends ISkill
+
+class_name SlashSkill
+
+func execute(args := []):
+	var attacker : Battler = args[0]
+	var victim : Battler = args[1]
+	
+	var attackerReference : BattlerReference = attacker.battlerReference
+	
+	var attackersStrength = 100
+	
+	attackerReference.goToEntity(victim)
+	yield(attackerReference.tween, "tween_completed")
+	
+	#I am going to use "Animation Player" for this.
+	#Attacker hits the victim.
+	yield(get_tree().create_timer(0.65), "timeout")
+	
+	#Victim takes some damage.
+	victim.takeDamage(attackersStrength)
+	
+	#Attacker returns to his place after attacking.
+	attackerReference.returnToStart()
+	yield(attackerReference.tween, "tween_completed")
+	
+	turnQueue.skipTurn()
