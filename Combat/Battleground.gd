db6036aaa287463fd971cc4b4433e608b5ea8d68
Eren Oğuz <65904273+Threnkost@users.noreply.github.com>
2020-08-09 21:07:12 +0300

    Add files via upload

--- Combat/Battleground.gd
+++ Combat/Battleground.gd
@@ -3,22 +3,54 @@ extends Node2D
 signal victory
 signal defeat
 
+onready var _enemyPoints = $SpawnPoints/EnemyParty
+onready var _playerPoints = $SpawnPoints/PlayerParty
+
 onready var enemies : Array = []
 onready var allies : Array = []
 
 func _ready():
+	var playerParty = []
+	var enemyParty = []
+	
+	for i in 3:
+		var PLAYER_CHARACTER = load("res://Combat/Characters/Character1.tscn").instance()
+		var ENEMY_CHARACTER = load("res://Combat/Characters/Monster1.tscn").instance()
+		
+		playerParty.append(PLAYER_CHARACTER)
+		enemyParty.append(ENEMY_CHARACTER)
+	
+	initializeTeams(playerParty, enemyParty)
 	enterBattle()
 
+func initializeTeams(playerParty : Array, enemyParty : Array) -> void:
+#	if playerParty.size() > 4 or enemyParty.size() > 4:
+#		print("\nBattleground.gd -> Error! Each party may have 4 members only!\n")
+#		assert(false)
+#		return
+#
+	enemies.clear()
+	allies.clear()
+	
+	enemies += enemyParty
+	allies += playerParty
+
 func enterBattle():
-	for battler in $TurnQueue.get_children():
-		if battler is Enemy:
-			enemies.append(battler)
-		elif battler is Ally:
-			allies.append(battler)
+	if enemies.empty() or allies.empty():
+		print("\nBattleground.gd -> Error! A team is completely empty.")
+		print("Battleground.gd -> First, initialize teams by 'initializeTeams(Array, Array)'!\n")
+		assert(false)
+		return
+	for allyIndex in allies.size():
+		allies[allyIndex].global_position = _playerPoints.get_child(allyIndex).global_position
+		$TurnQueue.add_child(allies[allyIndex])
+	for enemyIndex in enemies.size():
+		enemies[enemyIndex].global_position = _enemyPoints.get_child(enemyIndex).global_position
+		$TurnQueue.add_child(enemies[enemyIndex])
+		
 	$TurnQueue.initialize()
 
 func _on_TurnQueue_turn_changed(activeBattler):
-	print("yes")
 	var node = get_node("UI/Panel/Scroll/Targets")
 	for child in node.get_children():
 		child.queue_free()
